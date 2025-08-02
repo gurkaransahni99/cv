d842dbb33b3a537da291c7930a52b22a8d522ca3
Gurkaran Singh Sahni <gurkaransahni@gmail.com>
2025-08-02 19:43:12 +0530

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -13,7 +13,7 @@ export const RESUME_DATA = {
   personalWebsiteUrl: "https://github.com/gurkaransahni99",
   contact: {
     email: "gurkaran.sahni@gmail.com",
-    tel:,
+    tel: +91,
     social: [
       {
         name: "LinkedIn",
